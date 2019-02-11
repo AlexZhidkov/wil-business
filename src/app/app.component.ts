1db1d416767679f4d8192cbce2d9312339d9feb4
Alexander Zhidkov <azhidkov@gmail.com>
2019-02-11 18:18:46 +0800

    sign in user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from './services/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'wil-business';
+
+  constructor(public authService: AuthService) { }
+
+  login() {
+    this.authService.loginWithGoogle();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
